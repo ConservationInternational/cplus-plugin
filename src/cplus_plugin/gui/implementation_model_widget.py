da1f44dc50ed4118b9055a91242d73c0a4bb1fd0
Kahiu <gkahiu@users.noreply.github.com>
2023-06-18 11:17:18 +0300

    Update implementation model UI.

--- src/cplus_plugin/gui/implementation_model_widget.py
+++ src/cplus_plugin/gui/implementation_model_widget.py
@@ -34,6 +34,8 @@ class ImplementationModelContainerWidget(QtWidgets.QWidget, WidgetUi):
         super().__init__(parent)
         self.setupUi(self)
 
+        self._items_loaded = False
+
         self.btn_add_one.setIcon(FileUtils.get_icon("play.svg"))
         self.btn_add_one.setToolTip(self.tr("Add selected NCS pathway"))
 
@@ -51,4 +53,36 @@ class ImplementationModelContainerWidget(QtWidgets.QWidget, WidgetUi):
         self.implementation_model_view.title = self.tr(
             "Implementation Models"
         )
+
+    def load(self):
+        """Load NCS pathways and implementation models to the views.
+
+        This function is idempotent as items will only be loaded once
+        on initial call.
+
+        """
+        if not self._items_loaded:
+            self._load_persisted_ncs_pathways()
+            self._items_loaded = True
+
+    def _load_persisted_ncs_pathways(self):
+        """Load default and user-defined NCS pathways."""
+        pass
+
+    def ncs_pathways(self):
+        """Returns the NCS pathway items in the NCS Pathways view.
+
+        :returns:
+        :rtype:
+        """
+        pass
+
+    def implementation_models(self):
+        """Returns the user-defined implementation models in the
+        Implementation Models view.
+
+        :returns:
+        :rtype:
+        """
+        pass
 
