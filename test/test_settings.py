c0cd81cc6c71017f92fed537b3a2d7795c1191f7
vermeulendivan <vermeulendivan@gmail.com>
2023-07-03 11:13:30 +0200

    Test python file for settings

--- test/test_settings.py
+++ test/test_settings.py
@@ -2,9 +2,8 @@ import unittest
 
 
 class CplusSettings(unittest.TestCase):
-
     def test_open_settings(self):
 
-        print('OPEN SETTINGS')
+        print("OPEN SETTINGS")
 
         self.assertEqual(True, False)  # add assertion here
