2199dccce1af2defb022306447ab2337811c2267
vermeulendivan <vermeulendivan@gmail.com>
2023-07-04 12:45:38 +0200

    Test for settings - WIP

--- test/test_settings.py
+++ test/test_settings.py
@@ -1,9 +1,19 @@
 import unittest
 
+from utilities_for_testing import get_qgis_app
+#from qgis.core import QgsProject
+#from qgis.gui import QgsMapCanvasLayer
+#from qgis_interface import QgisInterface
 
-class CplusSettings(unittest.TestCase):
+QGIS_APP = get_qgis_app()
+
+
+class CplusPluginSettingsTest(unittest.TestCase):
     def test_open_settings(self):
+        print('\n\n\nOPEN SETTINGS123')
 
-        print("OPEN SETTINGS")
+        print(str(get_qgis_app()))
 
         self.assertEqual(True, False)  # add assertion here
+
+        print('\n\n\nEND')
