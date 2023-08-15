11d324fe49b661bd996d3a8d0e8cb647ca5c029c
Kahiu <gkahiu@users.noreply.github.com>
2023-08-15 06:40:15 +0300

    Refactor report manager unit tests.

--- test/test_report_manager.py
+++ test/test_report_manager.py
@@ -25,19 +25,18 @@ QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
 class TestReportManager(TestCase):
     """Tests for the report manager."""
 
-    def setUp(self):
-        self.rpm = ReportManager()
-
     def failed_submit_without_base_dir(self):
         """Assert a failed job submit if the BASE_DIR has not been set."""
+        rpm = ReportManager()
         scenario_result = get_test_scenario_result()
-        report_submit = self.rpm.generate(scenario_result, QgsFeedback())
+        report_submit = rpm.generate(scenario_result, QgsFeedback())
         self.assertFalse(report_submit.status)
 
     def successful_submit_when_base_dir_set(self):
         """Assert a successful job submit when the BASE_DIR has been set."""
+        rpm = ReportManager()
         base_dir = os.path.normpath(f"{QtCore.QDir.homePath()}/cplus_base")
         settings_manager.set_value(Settings.BASE_DIR, str(base_dir))
         scenario_result = get_test_scenario_result()
-        report_submit = self.rpm.generate(scenario_result, QgsFeedback())
+        report_submit = rpm.generate(scenario_result, QgsFeedback())
         self.assertTrue(report_submit.status)
