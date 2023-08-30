47ab3f43c50a12072ea2ad0e9c46d7d0645052ca
Kahiu <gkahiu@users.noreply.github.com>
2023-08-30 09:51:57 +0300

    Black reformat.

--- test/model_data_for_testing.py
+++ test/model_data_for_testing.py
@@ -35,7 +35,7 @@ def get_valid_ncs_pathway() -> NcsPathway:
         LayerType.RASTER,
         True,
         carbon_paths=[],
-        carbon_coefficient=0.0
+        carbon_coefficient=0.0,
     )
 
 
@@ -128,5 +128,5 @@ NCS_PATHWAY_DICT = {
     "layer_type": 0,
     "user_defined": True,
     "carbon_paths": [],
-    "carbon_coefficient": 0.00
+    "carbon_coefficient": 0.00,
 }
