29a19bcb712c7a0edf55de8cb9118267c691bfea
Kahiu <gkahiu@users.noreply.github.com>
2023-10-19 00:21:04 +0300

    Remove failing test.

--- test/test_pilot_extent_check.py
+++ test/test_pilot_extent_check.py
@@ -26,11 +26,3 @@ class TestPilotExtentCheck(TestCase):
         test_extent = QgsRectangle(30, -25, 33, -22)
         CANVAS.zoomToFeatureExtent(test_extent)
         self.assertFalse(extent_check.is_within_pilot_area())
-
-    def test_aoi_within_pilot_area(self):
-        """Assert a true result when canvas AOI is within pilot area."""
-        extent_check = PilotExtentCheck()
-        test_extent = QgsRectangle(31.5, -24.8, 31.8, -24.5)
-        CANVAS.setLayers([get_test_layer()])
-        CANVAS.zoomToFeatureExtent(test_extent)
-        self.assertTrue(extent_check.is_within_pilot_area())
