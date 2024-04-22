b2acad395991a0f932aabe42c31be9f6bdf763ca
Kahiu <gkahiu@users.noreply.github.com>
2024-04-23 00:11:52 +0300

    Add raster validator test.

--- test/test_validation.py
+++ test/test_validation.py
@@ -5,22 +5,36 @@ Unit tests for data validation module.
 
 from unittest import TestCase
 
+from cplus_plugin.lib.validation.configs import raster_validation_config
+from cplus_plugin.lib.validation.feedback import ValidationFeedback
+from cplus_plugin.lib.validation.manager import ValidationManager
+from cplus_plugin.lib.validation.validators import DataValidator, RasterValidator
+from cplus_plugin.models.validation import RuleInfo, RuleType
+
 from model_data_for_testing import (
     get_activity,
     get_invalid_ncs_pathway,
     get_ncs_pathway_with_invalid_carbon,
     get_ncs_pathway_with_valid_carbon,
+    get_ncs_pathways,
     get_valid_ncs_pathway,
     VALID_NCS_UUID_STR,
 )
 
 
 class TestDataValidation(TestCase):
-    def setUp(self):
-        self.ncs = get_valid_ncs_pathway()
-        self.invalid_ncs = get_invalid_ncs_pathway()
+    """Tests for data validation module."""
 
-    def test_to_map_layer(self):
-        """Confirm that the map layer is not None."""
-        map_layer = self.ncs.to_map_layer()
-        self.assertIsNotNone(map_layer)
+    def test_raster_validator(self):
+        """Test if the input NCS datasets are raster layers."""
+        ncs_pathways = get_ncs_pathways()
+        rule_info = RuleInfo(RuleType.DATA_TYPE, raster_validation_config.rule_name)
+        feedback = ValidationFeedback()
+        feedback.current_rule = rule_info
+        raster_validator = DataValidator.create_rule_validator(
+            RuleType.DATA_TYPE, raster_validation_config, feedback
+        )
+        raster_validator.model_components = ncs_pathways
+
+        result = raster_validator.run()
+        self.assertTrue(result)
