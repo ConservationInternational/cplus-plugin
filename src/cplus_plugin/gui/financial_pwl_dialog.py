0e709632a77afc9ddb4dba727b604a8138a08e28
Kahiu <gkahiu@users.noreply.github.com>
2024-05-04 23:44:52 +0300

    Add financial value delegate.

--- src/cplus_plugin/gui/financial_pwl_dialog.py
+++ src/cplus_plugin/gui/financial_pwl_dialog.py
@@ -17,7 +17,7 @@ from qgis.core import (
 )
 from qgis.gui import QgsGui, QgsMessageBar
 
-from qgis.PyQt import QtGui, QtWidgets
+from qgis.PyQt import QtCore, QtGui, QtWidgets
 
 from qgis.PyQt.uic import loadUiType
 
@@ -29,12 +29,108 @@ from ..definitions.constants import (
 )
 from ..definitions.defaults import ICON_PATH, USER_DOCUMENTATION_SITE
 from .financial_npv_model import FinancialNpvModel
-from ..models.base import Activity
 from ..utils import FileUtils, open_documentation, tr
 
 WidgetUi, _ = loadUiType(
     os.path.join(os.path.dirname(__file__), "../ui/financial_pwl_dialog.ui")
 )
+
+
+class FinancialValueItemDelegate(QtWidgets.QStyledItemDelegate):
+    """
+    Delegate for ensuring only numbers are specified in financial value
+    fields.
+    """
+
+    def createEditor(
+        self,
+        parent: QtWidgets.QWidget,
+        option: QtWidgets.QStyleOptionViewItem,
+        idx: QtCore.QModelIndex,
+    ) -> QtWidgets.QLineEdit:
+        """Creates a line edit control whose input value is limited to numbers only.
+
+        :param parent: Parent widget.
+        :type parent: QtWidgets.QWidget
+
+        :param option: Options for drawing the widget in the view.
+        :type option: QtWidgets.QStyleOptionViewItem
+
+        :param idx: Location of the request in the data model.
+        :type idx: QtCore.QModelIndex
+
+        :returns: The editor widget.
+        :rtype: QtWidgets.QLineEdit
+        """
+        line_edit = QtWidgets.QLineEdit(parent)
+        line_edit.setFrame(False)
+        line_edit.setMaxLength(50)
+        validator = QtGui.QDoubleValidator()
+        validator.setDecimals(2)
+        line_edit.setValidator(validator)
+
+        return line_edit
+
+    def setEditorData(self, widget: QtWidgets.QWidget, idx: QtCore.QModelIndex):
+        """Sets the data to be displayed and edited by the editor.
+
+        :param widget: Editor widget.
+        :type widget: QtWidgets.QWidget
+
+        :param idx: Location in the data model.
+        :type idx: QtCore.QModelIndex
+        """
+        value = idx.model().data(idx, QtCore.Qt.EditRole)
+        if value is None:
+            widget.setText("")
+        else:
+            widget.setText(str(value))
+
+    def setModelData(
+        self,
+        widget: QtWidgets.QWidget,
+        model: QtCore.QAbstractItemModel,
+        idx: QtCore.QModelIndex,
+    ):
+        """Gets data from the editor widget and stores it in the specified
+        model at the item index.
+
+        :param widget: Editor widget.
+        :type widget: QtWidgets.QWidget
+
+        :param model: Model to store the editor data in.
+        :type model: QtCore.QAbstractItemModel
+
+        :param idx: Location in the data model.
+        :type idx: QtCore.QModelIndex
+        """
+        if not widget.text():
+            value = None
+        else:
+            value = float(widget.text())
+
+        model.setData(idx, value, QtCore.Qt.EditRole)
+
+    def updateEditorGeometry(
+        self,
+        widget: QtWidgets.QWidget,
+        option: QtWidgets.QStyleOptionViewItem,
+        idx: QtCore.QModelIndex,
+    ):
+        """Updates the geometry of the editor for the item with the given index,
+        according to the rectangle specified in the option.
+
+        :param widget: Widget whose geometry will be updated.
+        :type widget: QtWidgets.QWidget
+
+        :param option: Option containing the rectangle for
+        updating the widget.
+        :type option: QtWidgets.QStyleOptionViewItem
+
+        :param idx: Location of the widget in the data model.
+        :type idx: QtCore.QModelIndex
+        """
+        widget.setGeometry(option.rect)
 
 
 class FinancialPwlDialog(QtWidgets.QDialog, WidgetUi):
@@ -63,6 +159,10 @@ class FinancialPwlDialog(QtWidgets.QDialog, WidgetUi):
         # Set view model
         self._npv_model = FinancialNpvModel()
         self.tv_revenue_costs.setModel(self._npv_model)
+        self._revenue_delegate = FinancialValueItemDelegate()
+        self._costs_delegate = FinancialValueItemDelegate()
+        self.tv_revenue_costs.setItemDelegateForColumn(1, self._revenue_delegate)
+        self.tv_revenue_costs.setItemDelegateForColumn(2, self._costs_delegate)
 
         self.sb_num_years.valueChanged.connect(self.on_number_years_changed)
 
