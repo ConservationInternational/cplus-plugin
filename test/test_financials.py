e495df4df3aceeaf11ac624982836af04a3d644b
Kahiu <gkahiu@users.noreply.github.com>
2024-06-15 22:43:05 +0300

    Add test for NPV PWL layer creation.

--- test/test_financials.py
+++ test/test_financials.py
@@ -3,11 +3,26 @@
 Unit tests for financial NPV computations.
 """
 
+import os
 from unittest import TestCase
 
-from cplus_plugin.lib.financials import compute_discount_value
+from processing.core.Processing import Processing
 
-from model_data_for_testing import ACTIVITY_UUID_STR, get_activity_npv_collection
+from qgis.core import (
+    QgsProcessingContext,
+    QgsProcessingFeedback,
+    QgsProcessingMultiStepFeedback,
+)
+
+from cplus_plugin.conf import settings_manager, Settings
+from cplus_plugin.lib.financials import compute_discount_value, create_npv_pwls
+from cplus_plugin.utils import FileUtils
+
+from model_data_for_testing import (
+    ACTIVITY_UUID_STR,
+    get_activity_npv_collection,
+    get_ncs_pathways,
+)
 from utilities_for_testing import get_qgis_app
 
 
@@ -16,6 +31,9 @@ QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
 
 class TestFinancialNpv(TestCase):
     """Tests for financial NPV computations."""
+
+    def setUp(self) -> None:
+        Processing.initialize()
 
     def test_get_activity_npv_in_collection(self):
         """Test getting the activity NPV in the NPV collection."""
@@ -55,3 +73,51 @@ class TestFinancialNpv(TestCase):
         activity_npv_1 = npv_collection.activity_npv(ACTIVITY_UUID_STR)
         normalized_npv = round(activity_npv_1.params.normalized_npv, 4)
         self.assertEqual(normalized_npv, 0.0259)
+
+    def test_create_npv_pwl(self):
+        """Test the creation of an NPV PWL raster layer."""
+        npv_collection = get_activity_npv_collection()
+        npv_collection.update_computed_normalization_range()
+        _ = npv_collection.normalize_npvs()
+
+        npv_processing_context = QgsProcessingContext()
+        npv_feedback = QgsProcessingFeedback(False)
+        npv_multi_step_feedback = QgsProcessingMultiStepFeedback(
+            len(npv_collection.mappings), npv_feedback
+        )
+
+        ncs_pathway = get_ncs_pathways()[0]
+        reference_layer = ncs_pathway.to_map_layer()
+        reference_crs = reference_layer.crs()
+        reference_pixel_size = reference_layer.rasterUnitsPerPixelX()
+        reference_extent = reference_layer.extent()
+        reference_extent_str = (
+            f"{reference_extent.xMinimum()!s},"
+            f"{reference_extent.xMaximum()!s},"
+            f"{reference_extent.yMinimum()!s},"
+            f"{reference_extent.yMaximum()!s}"
+        )
+
+        base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+        settings_manager.set_value(Settings.BASE_DIR, base_dir)
+        FileUtils.create_pwls_dir(base_dir)
+
+        pwl_layer_path = None
+
+        def on_pwl_layer_created(activity_npv, pwl_path, algorithm, context, feedback):
+            nonlocal pwl_layer_path
+            assert pwl_path
+            pwl_layer_path = pwl_path
+
+        create_npv_pwls(
+            npv_collection,
+            npv_processing_context,
+            npv_multi_step_feedback,
+            npv_feedback,
+            reference_crs.authid(),
+            reference_pixel_size,
+            reference_extent_str,
+            on_pwl_layer_created,
+        )
+
+        self.assertIsNotNone(pwl_layer_path)
