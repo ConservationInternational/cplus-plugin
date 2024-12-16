56ce3700301affe05100c583dd5cdd5c6a6c941d
Kahiu <gkahiu@users.noreply.github.com>
2024-12-16 11:29:40 +0300

    Remove inapplicable defaults.

--- src/cplus_plugin/definitions/defaults.py
+++ src/cplus_plugin/definitions/defaults.py
@@ -245,23 +245,6 @@ IRRECOVERABLE_CARBON_API_URL = f"{BASE_API_URL}/reference_layer/carbon_calculati
 DEFAULT_BASE_COMPARISON_REPORT_NAME = "Scenario Comparison Report"
 MAXIMUM_COMPARISON_REPORTS = 10
 
-NPV_EXPRESSION_DESCRIPTION = (
-    "Calculates the financial NPV of the current "
-    "activity. This returns the equivalent of the "
-    "area of the current activity (in hectares) "
-    "and multiplies it by the NPV rate (US$/ha) "
-    "for the activity calculated via the NPV PWL "
-    "Manager.<br><b>NOTE: If the NPV is not defined "
-    "then the function will return -1.0.</b>"
-)
-PWL_IMPACT_EXPRESSION_DESCRIPTION = (
-    "Calculates the impact of the "
-    "current activity by multiplying "
-    "the area of the activity (in hectares) "
-    "by a user-defined number of jobs created per "
-    "hectare. The activity area will be "
-    "automatically populated during the computation."
-)
 MEAN_BASED_IRRECOVERABLE_CARBON_EXPRESSION_DESCRIPTION = (
     "Calculates the total irrecoverable carbon (tons C) of "
     "protection NCS pathways in an activity using the mean "
