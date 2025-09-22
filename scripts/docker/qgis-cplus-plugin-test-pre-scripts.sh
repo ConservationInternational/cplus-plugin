a8a6136b3d0b23ea278e330b5de318d0c1ab35ee
Jeff Osundwa <jeff@kartoza.com>
2025-09-22 13:06:34 +0300

    fix CI testing, for plotly

--- scripts/docker/qgis-cplus-plugin-test-pre-scripts.sh
+++ scripts/docker/qgis-cplus-plugin-test-pre-scripts.sh
@@ -6,3 +6,5 @@ qgis_setup.sh
 rm -rf  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/cplus_plugin
 ln -sf /tests_directory /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/cplus_plugin
 ln -sf /tests_directory /usr/share/qgis/python/plugins/cplus_plugin
+
+pip3 install -r /tests_directory/requirements-testing.txt
